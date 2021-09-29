bdad67ac714b1a690b0dfebcaf449804b5e2dad2
nubddev2001 <nubddev.2001@gmail.com>
2021-09-29 20:53:52 +0700

    Hash Path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import { HashRouter as Router,Switch,Route } from 'react-router-dom';
 import './App.css';
 import { Header,Footer } from './components/init'
 import Loadable from 'react-loadable';
