3810996390f7f6001bb1c173cdfa7d95058fd7dd
nubdev2001 <nubdev.2001@gmail.com>
2022-03-05 21:55:47 +0700

    Updates

--- src/components/init.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './layouts/Header'
-export { default as Footer } from './layouts/Footer'
-
-export { default as Home } from './pages/Home'
-export { default as Contact } from './pages/Contact'
-export { default as Profile } from './pages/Profile'
